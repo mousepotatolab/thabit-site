c6aa9113b6b9f32213cd166853bf0ee15ce71a12
matte-brown <matthew@mousepotato.co>
2023-03-20 13:57:19 -0700

    mobile-nav

--- components/navbar.js
+++ components/navbar.js
@@ -10,10 +10,22 @@ import logo from "../public/logo.svg";
 
 import { useSelector } from "react-redux";
 
+import { useState } from "react";
+
 export default function Navbar() {
   let activePage = useSelector((state) => state.activePage.value);
 
   console.log(activePage);
+
+  const [isOpenMobileNav, setOpenMobileNav] = useState(false);
+  const [isClosedMobileNav, setCloseMobileNav] = useState(true);
+  const [isDisplayed, setDisplay] = useState(true);
+  function toggleMobileNav() {
+    setOpenMobileNav(!isOpenMobileNav);
+
+    setCloseMobileNav(!isClosedMobileNav);
+    setDisplay(false);
+  }
 
   return (
     <div className="flex w-full m-auto justify-between container mt-10">
@@ -35,7 +47,7 @@ export default function Navbar() {
         viewport={{ once: true }}
         transition={{ duration: 0.3 }}
       >
-        <nav className="">
+        <nav className="desktop-nav">
           <ul className="flex items-center">
             <Link href="/projects">
               <li
@@ -85,6 +97,75 @@ export default function Navbar() {
             <ThemeToggle />
           </ul>
         </nav>
+        {!isOpenMobileNav && (
+          <div onClick={toggleMobileNav} className="mobile-nav-toggle">
+            <div className="top-line"></div>
+            <div className="bottom-line"></div>
+          </div>
+        )}
+        {isOpenMobileNav && (
+          <div onClick={toggleMobileNav} className="mobile-nav-toggle">
+            <div className="top-line-white"></div>
+            <div className="bottom-line-white"></div>
+          </div>
+        )}
+        {isOpenMobileNav && (
+          <nav className="mobile-nav">
+            <ul className="flex items-center mobile-nav">
+              <Link href="/projects">
+                <li
+                  onClick={toggleMobileNav}
+                  className={`${
+                    activePage === "projects" ? "nav-item-active" : "nav-item"
+                  } uppercase font-regular mb-4`}
+                >
+                  Projects
+                </li>
+              </Link>
+              <Link href="/services">
+                <li
+                  onClick={toggleMobileNav}
+                  className={`${
+                    activePage === "services" ? "nav-item-active" : "nav-item"
+                  } uppercase font-regular mb-4`}
+                >
+                  Services
+                </li>
+              </Link>
+              <Link href="/calendar">
+                <li
+                  onClick={toggleMobileNav}
+                  className={`${
+                    activePage === "calendar" ? "nav-item-active" : "nav-item"
+                  } uppercase font-regular mb-4`}
+                >
+                  Calendar
+                </li>
+              </Link>
+              <Link href="/resources">
+                <li
+                  onClick={toggleMobileNav}
+                  className={`${
+                    activePage === "resources" ? "nav-item-active" : "nav-item"
+                  } uppercase font-regular mb-4`}
+                >
+                  Resources
+                </li>
+              </Link>
+              <Link href="/contact">
+                <li
+                  onClick={toggleMobileNav}
+                  className={`${
+                    activePage === "contact" ? "nav-item-active" : "nav-item"
+                  } uppercase font-regular mb-4`}
+                >
+                  Contact
+                </li>
+              </Link>
+              <ThemeToggle />
+            </ul>
+          </nav>
+        )}
       </motion.div>
     </div>
   );
