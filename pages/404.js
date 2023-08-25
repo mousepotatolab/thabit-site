590343c8451b046bfd8612a3570ed88ccb0540d1
matte-brown <matthew@mousepotato.co>
2023-08-25 16:43:37 -0700

    initial commit fix 404

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,6 @@
 import React, { useEffect } from "react";
 
-import Image from "next/image";
-
-import lab from "../public/sixth.svg";
-
 import SuccessLayout from "@/components/successLayout";
-
-import { useAnimation, motion } from "framer-motion";
 
 import { useRouter } from "next/router";
 
@@ -16,31 +10,7 @@ export default function PageNotFound() {
     setTimeout(() => router.push("/"), 3500);
   });
 
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.6 }}
-    >
-      <div className="w-full mt-10 flex items-center col justify-center container m-auto">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, x: 0, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6 }}
-          className="mb-20"
-          // animate={{ x: 100, y: -100 }}
-        ></motion.div>
-        <h1 className="text-xl mb-30 max-660">
-          404
-          <br />
-          You have reached a page that does not exist. Check the url for issues.
-          Maybe we are in the lab working on this page
-        </h1>
-      </div>
-    </motion.div>
-  );
+  return <h1>404</h1>;
 }
 PageNotFound.getLayout = function getLayout(page) {
   return <SuccessLayout>{page}</SuccessLayout>;
--- pages/500.js
+++ pages/500.js
@@ -2,11 +2,7 @@ import React, { useEffect } from "react";
 
 import Image from "next/image";
 
-import lab from "../public/sixth.svg";
-
 import SuccessLayout from "@/components/successLayout";
-
-import { useAnimation, motion } from "framer-motion";
 
 import { useRouter } from "next/router";
 
@@ -16,30 +12,7 @@ export default function InternalError() {
     setTimeout(() => router.push("/"), 3500);
   });
 
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.6 }}
-    >
-      <div className="w-full mt-10 flex items-center col justify-center container m-auto">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, x: 0, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6 }}
-          className="mb-20"
-          // animate={{ x: 100, y: -100 }}
-        ></motion.div>
-        <h1 className="text-xl mb-30 max-660">
-          500
-          <br />
-          Internal server error
-        </h1>
-      </div>
-    </motion.div>
-  );
+  return <h1>500</h1>;
 }
 InternalError.getLayout = function getLayout(page) {
   return <SuccessLayout>{page}</SuccessLayout>;
